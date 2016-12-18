acd6536151f7d438f47a3588e9a141cf0217c595
cwlch <cwlch@163.com>
2016-12-18 14:46:01 +0800

    新建项目文件

---
+++ gulpfile.js
@@ -0,0 +1,123 @@
+/**
+ * Created by 201603310162 on 2016/5/11.
+ */
+var gulp = require('gulp')
+	,webpack = require('webpack')
+	,gutil = require('gulp-util')
+	,webpackConfig = require('./webpack.config.js')
+	,server = require('./httpserver.js')
+	,minimist = require("minimist")
+	,build = require("./build_config.json")
+	,clean = require("gulp-clean")
+	,path = require("path")
+	,paths = ['./src/*','./src/**/*'] //文件路劲
+	,argv = minimist(process.argv)
+	,stringSrc = function(filename, string) { //把特定的字符转成流
+		var src = require('stream').Readable({
+			objectMode: true
+		});
+		src._read = function() {
+			this.push(new gutil.File({
+				cwd: '',
+				base: '',
+				path: filename,
+				contents: new Buffer(string)
+			}));
+			this.push(null);
+		};
+		return src;
+	};
+
+
+//监听任务
+gulp.task('watch', function() {
+	gulp.watch(paths,['webpack']);
+});
+//清除文件任务
+gulp.task("clean",function () {
+	return gulp.src(build.dest).pipe(clean());
+});
+gulp.task('constants', function () {
+	//取出对应的配置信息
+	var envConfig, conConfig = '',serverPath = build.serverPath;
+	for(var i = 0; i < serverPath.length; i++){
+		if (argv.test) {
+			envConfig = serverPath[i].test;
+		} else if (argv.repro) {
+			envConfig = serverPath[i].repro;
+		} else if (argv.pro) {
+			envConfig = serverPath[i].pro;
+		} else {
+			envConfig = serverPath[i].dev;
+		}
+		conConfig += 'window.serverPath'+ i +' = ' + JSON.stringify(envConfig) +';';
+	}
+
+
+	// conConfig = 'window.serverPath = ' + JSON.stringify(envConfig) +';';
+
+	//base_url.js文件
+	return stringSrc('serverPath.js', conConfig)
+		.pipe(gulp.dest(path.resolve(__dirname ,build.src + '/js/common/') ));
+});
+gulp.task('staticPath',function () {
+
+	//取出对应的配置信息
+	var envConfig, conConfig,staticPath = build.staticPath;
+
+	if (argv.test) {
+		envConfig = staticPath.test;
+	} else if (argv.repro) {
+		envConfig = staticPath.repro;
+	} else if (argv.pro) {
+		envConfig = staticPath.pro;
+	} else {
+		envConfig = staticPath.dev;
+	}
+
+	conConfig = 'window.staticPath = ' + JSON.stringify(envConfig) +';';
+
+	//base_url.js文件
+	return stringSrc('staticPath.js', conConfig)
+		.pipe(gulp.dest(path.resolve(__dirname ,build.src + '/js/common/') ));
+});
+
+
+//webpack打包任务
+gulp.task('webpack', function(cb) {
+	webpack(webpackConfig, function(err, stats) {
+		console.log(stats.errors, stats.warnings);
+		if (err) throw new gutil.PluginError('webpack', err);
+		gutil.log("[webpack]", stats.toString({
+			colors: true
+		}));
+		cb();
+	});
+
+});
+
+gulp.task("help",function () {
+	console.log("所有打包输出口为:'" + build.dest + "'");
+	console.log("gulp --dev  |  开发环境,自动启动本地服务。端口为:" + build.config.port);
+	console.log("gulp --test  |  测试环境,请在test分支中运行");
+	console.log("gulp --repro  |  准生产环境,请在release分支中运行");
+	console.log("gulp --pro  |  生产环境,请在master分支中运行");
+});
+
+gulp.task("m-img",function () {
+	gulp.src( build.src+'/img/move_img/**' ).pipe( gulp.dest(build.dest +'/img/move_img/') );
+});
+
+// 运行任务
+gulp.task('default',['constants','clean'], function() {
+	if(!argv.dev){
+		gulp.start(['m-img','webpack','staticPath'],function () {
+			console.log("打包完毕")
+		});
+	}else{
+		gulp.start(['m-img','watch','webpack','staticPath'],function () {
+			console.log("打包完毕")
+		});
+		server();
+	}
+});
---
+++ webpack.config.js
@@ -0,0 +1,129 @@
+var webpack = require("webpack")
+	,HtmlwebpackPlugin = require('html-webpack-plugin')
+	,ExtractTextPlugin = require("extract-text-webpack-plugin")
+	,path = require("path")
+	,minimist = require("minimist")
+	,build = require("./build_config.json")
+	,ALIAS = {},COMMON = [],ENTRY = {},PLUGINS = []
+	,PATH_SRC = path.resolve(__dirname ,build.src)
+	,util
+	,argv = minimist(process.argv);
+//第三方库
+if(build.libs){
+	build.libs.forEach(function(i){
+		ALIAS[i.name] = path.resolve(PATH_SRC , i.src);
+	});
+}
+
+//公共文件打包 提取
+if(build.common){
+	build.common.forEach(function(i){
+		if(i.indexOf(".js") >= 0 ){
+			COMMON.push(path.resolve(PATH_SRC , i));
+		}else{
+			COMMON.push(i);
+		}
+	});
+}
+ENTRY.common = COMMON;
+PLUGINS.push(new webpack.optimize.CommonsChunkPlugin('common', './js/common.js'));
+
+
+//内部UI组件
+if(build.ui){
+	build.ui.forEach(function(i){
+		ALIAS[i.name] =  path.resolve(PATH_SRC , i.src);
+	});
+}
+
+//设置入口文件JS
+for(var x in build.entry){
+	ENTRY[x] = path.resolve(PATH_SRC , build.entry[x]);
+}
+
+
+
+
+// 加入,提取入口HTML页面插件
+for(var page = build.htmlwebpack, i = page.length; i--;){
+	var me = page[i];
+	me.template = path.resolve(PATH_SRC, me.filename);
+	if(me.common)me.chunks.unshift("common");
+	PLUGINS.push(new HtmlwebpackPlugin(me));
+}
+
+//css提取插件
+PLUGINS.push(new ExtractTextPlugin("./css/[name].css",{ allChunks : true,resolve : ['modules'] }));
+
+PLUGINS.push(new webpack.ProvidePlugin(build.providePlugin));
+
+
+
+util = {
+	entry: ENTRY,
+	output: {//静态文件路径设置
+		path: build.dest,
+		publicPath: '/./' + build.dest + "/",
+		chunkFilename : 'js/[name].js',
+		filename: 'js/[name].js'
+	},
+	module: {//模版解析
+		loaders: [
+			{
+				test: /\.vue$/,
+				loader: 'vue'
+			}, {
+				test: /\.html$/,
+				loader: 'html'
+			},
+			{
+				test: /\.js?$/,
+				loader:  'babel' ,
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015'],
+					cacheDirectory: true,
+					plugins: ['transform-runtime'],
+					compact: false
+				}
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				loader: 'url-loader?limit=12819&name=img/[name].[ext]'
+			},
+			{
+				test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'file-loader?name=fonts/[name].[ext]'
+			}/*,{
+				test: /\.css$/,
+				loader:ExtractTextPlugin.extract("style", "css")
+			}, { //编译SCSS生成link链接
+				test: /\.scss$/,
+				loader: ExtractSCSS.extract('style', 'css!sass?sourceMap')
+			}*/, { //编译SCSS生成link链接
+				test: /\.scss$/,
+				loader: ExtractTextPlugin.extract('style', 'css!sass?sourceMap')
+			}
+		]
+	},
+	resolve : {//第三方库
+		root: PATH_SRC //绝对路径
+		,extensions: ['', '.js','.jsx', '.json',".css"]
+		,modulesDirectories: [
+			'node_modules'
+		]
+		,alias: ALIAS
+	},
+	plugins :PLUGINS
+	// ,devtool : '#source-map'
+};
+if(argv.dev){
+	util.devtool = '#source-map';
+}else{
+	//压缩js
+	util.plugins.push(new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}));
+}
+
+
+
+module.exports = util;
